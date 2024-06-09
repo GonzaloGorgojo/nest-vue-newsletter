552cce26e39939c41e657746a17b0480a5a2dee3
gon <gongorgojo@gmail.com>
2024-06-09 19:48:00 -0300

    feat(test-backend): added 2 end to end test cases, and setted configuration

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Test app E2E', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,31 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('Should login correctly', async () => {
+    const payload = {
+      username: 'admin@gmail.com',
+      password: 'admin1234',
+    };
+
+    await request(app.getHttpServer())
+      .post('/auth/login')
+      .send(payload)
+      .expect(200);
+  });
+
+  it('Should get unauthorized when login with invalid credentials', async () => {
+    const payload = {
+      username: 'wrong@gmail.com',
+      password: 'wrong1234',
+    };
+
+    await request(app.getHttpServer())
+      .post('/auth/login')
+      .send(payload)
+      .expect(401);
   });
 });
